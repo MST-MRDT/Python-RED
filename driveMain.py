b4c8899646d74ba4f2e8b17d99e30dce437bd86b
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-01-31 14:32:34 -0600

    Created main run file for drive station

---
+++ driveMain.py
@@ -0,0 +1,27 @@
+import pyglet
+from modules import xboxController
+
+myController = xboxController.XboxController()
+
+
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.text = str(myController.getButton(0))
+    label.draw()
+
+@window.event
+def update(value):
+    window.clear()
+    label.text = str(myController.getButton(0))
+    label.draw()
+
+pyglet.clock.schedule_interval(update, 1/120.0)
+pyglet.app.run()
