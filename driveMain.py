d61ded77c776571a95456660cfe9a651910f239d
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-02-02 14:00:44 -0600

    Added proper drive power labels, percents

--- driveMain.py
+++ driveMain.py
@@ -5,23 +5,38 @@ myController = xboxController.XboxController()
 
 
 window = pyglet.window.Window(resizable=True, caption='DriveRED')
-label = pyglet.text.Label('Hello, world',
+
+leftLabel = pyglet.text.Label("HI",
                           font_name='Times New Roman',
                           font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+                          x=10, y=50)
 
-@window.event
-def on_draw():
-    window.clear()
-    label.text = str(myController.getButton(0))
-    label.draw()
+rightLabel = pyglet.text.Label("HI",
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=10, y=10)
+
+
+def toPercent(value):
+    return str(round(value * 100)) + "%"
+
+#@window.event
+#def on_draw():
 
 @window.event
 def mainUpdate(value):
     window.clear()
-    label.text = str(myController.getButton(0))
-    label.draw()
 
-pyglet.clock.schedule_interval(mainUpdate, 1/120.0)
+    leftTread = myController.getAxis(1)
+    rightTread = myController.getAxis(3)
+
+    print(toPercent(leftTread))
+
+    leftLabel.text = "Left Power: " + toPercent(leftTread)
+    leftLabel.draw()
+
+    rightLabel.text = "Right Power: " + toPercent(rightTread)
+    rightLabel.draw()
+
+pyglet.clock.schedule_interval(mainUpdate, 1/60.0)
 pyglet.app.run()
